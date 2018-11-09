9bab14c8626ccf77dcc602c04347e079a04050ac
shankar <shankar.edgi01@gmail.com>
2018-11-09 23:01:11 +0530

    Updated target folder settings

--- onlinebackend/src/main/java/net/usi/onlinebackend/App.java
+++ onlinebackend/src/main/java/net/usi/onlinebackend/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello welcome to online shopping backend" );
+        System.out.println( "welcome to online shopping backend" );
     }
 }
